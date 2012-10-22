a4e5d867112adb3eb1d264c19c4755012d86f17a
Benjamin DOMERGUE <thebenjiman@TheHairyMan.local>
2012-10-22 22:43:10 +0200

    Initial Commit

---
+++ StudentGrade/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  StudentGrade
+//
+//  Created by Benjamin DOMERGUE on 22/10/12.
+//  Copyright (c) 2012 Benjamin DOMERGUE. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
